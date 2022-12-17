deb4dfbbb727c3e72d660fe342a0ebeb98aca7fe
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-12-17 11:04:38 +0300

    bind to a local address

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use std::net::TcpListener;
 use std::io::Write;
 use std::thread;
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1").unwrap();
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Listening started, ready to accept!");
+    for data in listener.incoming(){
+        thread::spawn(|| {
+            let mut data = data.unwrap();
+            data.write(b"Hello, World!\r\n").unwrap();
+        });
+
+    }
 }
